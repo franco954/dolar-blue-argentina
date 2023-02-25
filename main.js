444a9ea4a2e2de76cf16b1e2189cac7a207d9655
franco954 <franconunez500@gmail.com>
2023-02-25 00:30:46 -0300

    sale price + buy price

--- main.js
+++ main.js
@@ -4,16 +4,12 @@
 const insertDataIntoPage = (pVenta, pCompra) => {
 
 
-    document.querySelector('#dolar-blue-precio').innerHTML = `<h2 class="text-info"> <strong> 1 USD = ${pCompra} pesos </strong></h>`
-
+    document.querySelector('#dolar-blue-precio').innerHTML = `<h2 class="text-info"> <strong> 1 USD = ${pCompra} ARS </strong></h2>`
+    document.querySelector('#dolar-blue-market-compra').innerHTML = `<strong> ${pCompra}  </strong>`
+    document.querySelector('#dolar-blue-market-venta').innerHTML = `<strong> ${pVenta}  </strong>`
 
 
 }
-
-
-
-
-
 
 
 fetch("https://criptoya.com/api/buenbit/dai/ars")
